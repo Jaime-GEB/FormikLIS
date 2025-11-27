0dba4f43dc4734649f52d42cf6bb00fa275b14da
Jaime-GEB <jaimegomez@inade.es>
2025-11-27 10:50:58 +0100

    Varios  contactos/slug

--- PruebaTecnica/src/modules/Agendas/components/AgendasForm/AgendasForm.tsx
+++ PruebaTecnica/src/modules/Agendas/components/AgendasForm/AgendasForm.tsx
@@ -79,97 +79,117 @@ const initialValues: FormValues = {
             >
               {({ values, handleChange, handleBlur, errors, touched }) => (
                 <Form>
-                  <Box mb={2}>
-                    <TextField
-                      fullWidth
-                      id="slug"
-                      name="slug"
-                      label="Slug"
-                      value={values.slug}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      error={touched.slug && Boolean(errors.slug)}
-                      helperText={touched.slug && errors.slug}
-                    />
-                  </Box>
-
-                  <FieldArray name="contacts">
-                        {() => {
+                    <Box mb={2}>
+                        <TextField
+                        fullWidth
+                        id="slug"
+                        name="slug"
+                        label="Slug"
+                        value={values.slug}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        error={touched.slug && Boolean(errors.slug)}
+                        helperText={touched.slug && errors.slug}
+                        />
+                    </Box>
+                    <h3>Contacto</h3>
+                    <FieldArray name="contacts">
+                        {({ push, remove }) => {
                             const { contacts } = values;
-
                             return (
                                 <div>
-
                                     {contacts.map((contact, index) => (
-                                        <div key={index}>
-                                            <TextField
-                                                fullWidth
-                                                id={`contacts.${index}.name`}
-                                                name={`contacts.${index}.name`}
-                                                label="Nombre Contacto"
-                                                value={values.contacts[index].name}
-                                                onChange={handleChange}
-                                                onBlur={handleBlur}
-                                                error={Boolean(getIn(touched, `contacts.${index}.name`) && getIn(errors, `contacts.${index}.name`))}
-                                                helperText={getIn(touched, `contacts.${index}.name`) && getIn(errors, `contacts.${index}.name`) ? String(getIn(errors, `contacts.${index}.name`)) : undefined}
-                                                margin="normal"
-                                            />
-                                            <TextField
-                                                fullWidth
-                                                id={`contacts.${index}.phone`}
-                                                name={`contacts.${index}.phone`}
-                                                label="Teléfono"
-                                                value={values.contacts[index].phone}
-                                                onChange={handleChange}
-                                                onBlur={handleBlur}
-                                                error={Boolean(getIn(touched, `contacts.${index}.phone`) && getIn(errors, `contacts.${index}.phone`))}
-                                                helperText={getIn(touched, `contacts.${index}.phone`) && getIn(errors, `contacts.${index}.phone`) ? String(getIn(errors, `contacts.${index}.phone`)) : undefined}
-                                                margin="normal"
-                                            />
-                                            <TextField
-                                                fullWidth
-                                                id={`contacts.${index}.email`}
-                                                name={`contacts.${index}.email`}
-                                                label="Email"
-                                                value={values.contacts[index].email}
-                                                onChange={handleChange}
-                                                onBlur={handleBlur}
-                                                error={Boolean(getIn(touched, `contacts.${index}.email`) && getIn(errors, `contacts.${index}.email`))}
-                                                helperText={getIn(touched, `contacts.${index}.email`) && getIn(errors, `contacts.${index}.email`) ? String(getIn(errors, `contacts.${index}.email`)) : undefined}
-                                                margin="normal"
-                                            />
-                                            <TextField
-                                                fullWidth
-                                                id={`contacts.${index}.address`}
-                                                name={`contacts.${index}.address`}
-                                                label="Dirección"
-                                                value={values.contacts[index].address}
-                                                onChange={handleChange}
-                                                onBlur={handleBlur}
-                                                error={Boolean(getIn(touched, `contacts.${index}.address`) && getIn(errors, `contacts.${index}.address`))}
-                                                helperText={getIn(touched, `contacts.${index}.address`) && getIn(errors, `contacts.${index}.address`) ? String(getIn(errors, `contacts.${index}.address`)) : undefined}
-                                                margin="normal"
-                                            />
-                                            <TextField
-                                                fullWidth
-                                                id={`contacts.${index}.id`}
-                                                name={`contacts.${index}.id`}
-                                                label="ID"
-                                                type="number"
-                                                value={values.contacts[index].id}
-                                                onChange={handleChange}
-                                                onBlur={handleBlur}
-                                                error={Boolean(getIn(touched, `contacts.${index}.id`) && getIn(errors, `contacts.${index}.id`))}
-                                                helperText={getIn(touched, `contacts.${index}.id`) && getIn(errors, `contacts.${index}.id`) ? String(getIn(errors, `contacts.${index}.id`)) : undefined}
-                                                margin="normal"
-                                            />
-                                        </div>
+                                        <Box key={index}>
+                                            <Box>
+                                                <Box>
+                                                    <TextField
+                                                        fullWidth
+                                                        id={`contacts.${index}.name`}
+                                                        name={`contacts.${index}.name`}
+                                                        label="Nombre Contacto"
+                                                        value={values.contacts[index].name}
+                                                        onChange={handleChange}
+                                                        onBlur={handleBlur}
+                                                        error={Boolean(getIn(touched, `contacts.${index}.name`) && getIn(errors, `contacts.${index}.name`))}
+                                                        helperText={getIn(touched, `contacts.${index}.name`) && getIn(errors, `contacts.${index}.name`) ? String(getIn(errors, `contacts.${index}.name`)) : undefined}
+                                                        margin="normal"
+                                                    />
+                                                    <TextField
+                                                        fullWidth
+                                                        id={`contacts.${index}.phone`}
+                                                        name={`contacts.${index}.phone`}
+                                                        label="Teléfono"
+                                                        value={values.contacts[index].phone}
+                                                        onChange={handleChange}
+                                                        onBlur={handleBlur}
+                                                        error={Boolean(getIn(touched, `contacts.${index}.phone`) && getIn(errors, `contacts.${index}.phone`))}
+                                                        helperText={getIn(touched, `contacts.${index}.phone`) && getIn(errors, `contacts.${index}.phone`) ? String(getIn(errors, `contacts.${index}.phone`)) : undefined}
+                                                        margin="normal"
+                                                    />
+                                                    <TextField
+                                                        fullWidth
+                                                        id={`contacts.${index}.email`}
+                                                        name={`contacts.${index}.email`}
+                                                        label="Email"
+                                                        value={values.contacts[index].email}
+                                                        onChange={handleChange}
+                                                        onBlur={handleBlur}
+                                                        error={Boolean(getIn(touched, `contacts.${index}.email`) && getIn(errors, `contacts.${index}.email`))}
+                                                        helperText={getIn(touched, `contacts.${index}.email`) && getIn(errors, `contacts.${index}.email`) ? String(getIn(errors, `contacts.${index}.email`)) : undefined}
+                                                        margin="normal"
+                                                    />
+                                                    <TextField
+                                                        fullWidth
+                                                        id={`contacts.${index}.address`}
+                                                        name={`contacts.${index}.address`}
+                                                        label="Dirección"
+                                                        value={values.contacts[index].address}
+                                                        onChange={handleChange}
+                                                        onBlur={handleBlur}
+                                                        error={Boolean(getIn(touched, `contacts.${index}.address`) && getIn(errors, `contacts.${index}.address`))}
+                                                        helperText={getIn(touched, `contacts.${index}.address`) && getIn(errors, `contacts.${index}.address`) ? String(getIn(errors, `contacts.${index}.address`)) : undefined}
+                                                        margin="normal"
+                                                    />
+                                                    <TextField
+                                                        fullWidth
+                                                        id={`contacts.${index}.id`}
+                                                        name={`contacts.${index}.id`}
+                                                        label="ID"
+                                                        type="number"
+                                                        value={values.contacts[index].id}
+                                                        onChange={handleChange}
+                                                        onBlur={handleBlur}
+                                                        error={Boolean(getIn(touched, `contacts.${index}.id`) && getIn(errors, `contacts.${index}.id`))}
+                                                        helperText={getIn(touched, `contacts.${index}.id`) && getIn(errors, `contacts.${index}.id`) ? String(getIn(errors, `contacts.${index}.id`)) : undefined}
+                                                        margin="normal"
+                                                    />
+                                                </Box>
+
+                                                <Button
+                                                    variant="outlined"
+                                                    color="error"
+                                                    onClick={() => remove(index)}
+                                                    style={{ marginTop: '10px' }}
+                                                >
+                                                    Eliminar contacto
+                                                </Button>
+                                            </Box>                 
+                                        
+                                            <Button
+                                                variant="outlined"
+                                                color="primary"
+                                                onClick={() =>
+                                                  push({ name: '', phone: '', email: '', address: '', id: 0 })
+                                                }
+                                            >
+                                              Añadir contacto
+                                            </Button>
+                                        </Box>
                                     ))}
                                 </div>
                             );
                         }}
                     </FieldArray>
-
                   <Box mt={3}>
                     <Button type="submit"fullWidth variant="contained" color="success">
                       Enviar
